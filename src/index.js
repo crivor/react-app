2a472ee54a1916ef63ca61757df041ae1d9c1ae6
Steven Foster <steven@stevenfoster.net>
2020-02-20 10:29:13 -0800

    refactored component with dynamic content from props

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ class Message extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello World</h1>
+                <h1 style={{color: this.props.color}}>{this.props.msg}</h1>
+                <p>Check back for more in {this.props.minutes} minutes</p>
             </div>
         )
     }
 }
 
-ReactDOM.render(<Message />, document.getElementById('root'))
+ReactDOM.render(<Message color="blue" msg="Hello World" minutes={5}/>, document.getElementById('root'))
